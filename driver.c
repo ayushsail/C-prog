4546940d39f312a0e0ca4cb15f994c1cd6dc1840
ayushsail <ayushsail888@gmail.com>
2025-07-20 13:06:29 +0530

    driver.c

---
+++ driver.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a;
+    char g[10], m[10];
+    char fn[10], ln[10];
+    printf("\nEnter your first name:");
+    scanf("%s", fn);
+    printf("\nEnter your last name:");
+    scanf("%s", ln);
+    printf("\nEnter your age:");
+    scanf("%d", &a);
+    printf("\nEnter your gender (male/female):"); 
+    scanf(" %s", g);
+    printf("\nAre you married? (yes/no):");
+    scanf("%s", m);
+    if(strcmp(m, "yes") == 0)
+        printf("You are insured.");
+    
+    else 
+        if(a>30 && strcmp(g, "male") == 0)
+            printf("\nYou are insured");
+        else if (a>25 && strcmp(g, "female") == 0)
+            printf("\nYou are insured.");
+        else
+            printf("\nYou are not insure.");
+
+    
+    return 0;
+}  
