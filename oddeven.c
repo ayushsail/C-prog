ab5eb7b073ba5959c084310dd9ed8400af709321
ayushsail <ayushsail888@gmail.com>
2025-07-21 07:09:25 +0530

    oddeven.c

--- oddeven.c
+++ oddeven.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
+
+int main() {
     int a;
+    printf("\nEnter a number:");
+    scanf("%d", &a);
+    if((a%2) == 0)
+    {
+        printf("The number is even.");
+        
+    }
+    else if(a != 0)
+    {
+        printf("The number is Zero");
+    }
+    else
+    {
+        printf("Thr number is odd.");
+    }
     return 0;
 }
