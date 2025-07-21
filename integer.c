7d759acc810381c4b7c606c59577f9e48c3a7b5d
ayushsail <ayushsail888@gmail.com>
2025-07-21 08:11:51 +0530

    integer.c

--- integer.c
+++ integer.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
     int a;
     printf("\nEnter a number:");
     scanf("%d", &a);
-    if(a < 0)
-        printf("\nThe number is negative");
-        
-    else if(a > 0)
-        printf("\nThe number is positive");
-
-    else
-        printf("\nThe number is zero");  
+    if(a==0){
+        printf("\nThe number is zero.");
+    }
+    else if(a>0){
+        printf("\nThe number is positive.");
+    }
+    else if(a<0){
+        printf("\nThe number is negative.");
+    }
+    else{
+        printf("\nInvalid number.");
+    }
     return 0;
 }
+
