f13fb6b9eef3a8eda73053310fc59f7e17dabbbf
ayushsail <ayushsail888@gmail.com>
2025-07-21 06:59:26 +0530

    oddeven.c

--- oddeven.c
+++ oddeven.c
@@ -2,15 +2,5 @@
 int main()
 {
     int a;
-    printf("\nEnter a number:");
-    scanf("%d", &a);
-    if(a>0)
-        printf("\nThe number is positive.");
-    else if(a<0)
-        printf("\nThe number is negative.");
-    else if (a==0)
-        printf("\nThe number is zero");
-    else
-        printf("\nInvalid input.");
     return 0;
 }
