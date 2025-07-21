83b94cb9a9eccd17a6fce6a9c2de2134988da2f1
ayushsail <ayushsail888@gmail.com>
2025-07-21 08:15:03 +0530

    month.c

---
+++ month.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("\nEnter three numbers:");
+    scanf("%d%d%d", &a, &b, &c);
+    if(a > b || a > c){
+        printf("\n%d is greatest of three", a);
+    }
+    else if(b > a || b > c){
+        printf("\n%d is greatest of three", b);
+    }
+    else if(c > a || c > b){
+        printf("\n%d is greatest of three", c);
+    }
+    else{
+        printf("\nInvalid statement");
+    }
+    return 0;
+}
