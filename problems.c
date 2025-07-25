220df6da0dbbced7c36e5ea2c7fb445ef44cef11
ayushsail <ayushsail888@gmail.com>
2025-07-25 19:54:35 +0530

    problems.c

---
+++ problems.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() 
+{
+    // // print even number till user input
+    // int n, i=2;
+    // printf("\nEnter a number between 2 to 100:");
+    // scanf("%d", &n);
+    // if(n>=2 && n<=100)
+    // {
+    //     while(i<=n)
+    //     {
+    //         printf("\n%d", i);
+    //         i = i+2;
+    //     }
+    // }  
+    // else{
+    //     printf("\nInvalid Input!!!!");
+    // }
+    
+    
+    // // print odd number till user input
+    // int n, i=1;
+    // printf("\nEnter a number between 2 to 100:");
+    // scanf("%d", &n);
+    // if(n>=2 && n<=100)
+    // {
+    //     while(i<=n)
+    //     {
+    //         printf("\n%d", i);
+    //         i = i+2;
+    //     }
+    // }  
+    // else{
+    //     printf("\nInvalid Input!!!!");
+    // }
+
+    return 0;
+}
